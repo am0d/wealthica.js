4f9a362352b1dc94d4a45a6806d33bf8a1b33993
Damien Schoof <damien.schoof@gmail.com>
2022-02-22 14:20:57 -0500

    Collect *all* the holdings from *all* the tabs.

--- src/index.ts
+++ src/index.ts
@@ -60,6 +60,14 @@ function getTabWithName(
   return tabPanel;
 }
 
+function tryOrDefault<T>(callback: () => T, defaultValue: T): T {
+  try {
+    return callback();
+  } catch {
+    return defaultValue;
+  }
+}
+
 function getHoldingsFromTable(
   $: CheerioAPI,
   section: Cheerio<Element>
@@ -68,59 +76,69 @@ function getHoldingsFromTable(
     findFirstElement(section, "th p", "Couldn't find section header").text()
   );
   const tableRows = section.find("tbody tr");
-  return _.flatten(
-    tableRows.map((_i, el) => {
-      const row = $(el);
-      const fundNameAnchor = findFirstElement(
-        row,
-        "td p:first-child a:first-child *",
-        "Couldn't find fund name"
-      );
-      const fundName = _.trim(
-        (fundNameAnchor[0]?.nextSibling as Text).nodeValue
-      );
-      const tabContents = findFirstElement(
-        row,
-        ".tab-content",
-        "Couldn't find tab content"
-      );
-      const fundUnits = parseFloat(
-        findFirstElement(
-          tabContents,
-          'li:contains("Number of units")',
-          "Couldn't find number of units"
-        )
-          .text()
-          .replace("Number of units: ", "")
-      );
-      const securitySymbol = (
-        findFirstElement(
-          tabContents,
-          'a:contains("More information")',
-          "Couldn't find more information link"
-        ).attr("href") ?? ""
-      )
-        .split("/")
-        .slice(-1)[0];
-      const marketValue = parseFloat(
-        findFirstElement(
+  return _.compact(
+    _.flatten(
+      tableRows.map((_i, el) => {
+        // try {
+        const row = $(el);
+        const fundNameAnchor = findFirstElement(
           row,
-          "td:last-child .js-money-switch-1",
-          "Couldn't find market value"
+          "td p:first-child a:first-child *",
+          "Couldn't find fund name"
+        );
+        const fundName = _.trim(
+          (fundNameAnchor[0]?.nextSibling as Text).nodeValue
+        );
+        const tabContents = findFirstElement(
+          row,
+          ".tab-content",
+          "Couldn't find tab content"
+        );
+        const fundUnits = tryOrDefault(
+          () =>
+            parseFloat(
+              findFirstElement(
+                tabContents,
+                'li:contains("Number of units")',
+                "Couldn't find number of units"
+              )
+                .text()
+                .replace("Number of units: ", "")
+            ),
+          0
+        );
+        const securitySymbol = (
+          findFirstElement(
+            tabContents,
+            'a:contains("More information")',
+            "Couldn't find more information link"
+          ).attr("href") ?? ""
         )
-          .text()
-          .replace(/[,\$]/g, "")
-      );
-      return {
-        security: {
-          type: tableCategory,
-          name: fundName,
-          symbol: securitySymbol,
-        },
-        quantity: fundUnits,
-        market_value: marketValue,
-      } as IHolding;
-    })
+          .split("/")
+          .slice(-1)[0];
+        const marketValue = parseFloat(
+          findFirstElement(
+            row,
+            'td:last-child span[class|="js-money-switch"]',
+            "Couldn't find market value"
+          )
+            .text()
+            .replace(/[,\$]/g, "")
+        );
+        return {
+          security: {
+            type: tableCategory,
+            name: fundName,
+            symbol: securitySymbol,
+          },
+          quantity: fundUnits,
+          market_value: marketValue,
+        } as IHolding;
+        // } catch {
+        //   return null;
+        // }
+      })
+    )
   );
 }
 
@@ -131,28 +149,40 @@ async function parseHoldings(page: string): Promise<IHolding[]> {
     "h2:contains('Details by contribution type')",
     "Couldn't find holdings section"
   ).next();
+  const topLevelTabs = findFirstElement(
+    holdingsSection,
+    ".tabList",
+    "Couldn't find top level tabs"
+  );
   // I have 3 tabs in this section - Regular, Voluntary, Employer
-  const currentHoldingsTab = getTabWithName(holdingsSection, "Regular");
-  const currentAllocationsTab = getTabWithName(
-    currentHoldingsTab,
-    "Current allocation"
-  );
-  const tables = currentAllocationsTab.find(".tableListBlock");
-  const holdings = _.flatten(
-    tables.map((_i, el) => getHoldingsFromTable($, $(el)))
-  );
-  console.log(JSON.stringify(holdings));
+  const allHoldings = topLevelTabs.find('li[role="tab"]').map(function () {
+    const tabName = _.trim($(this).text());
+    // const tabName = "Employer";
+    const holdingsTab = getTabWithName(holdingsSection, tabName);
+    const currentAllocationsTab = getTabWithName(
+      holdingsTab,
+      "Current allocation"
+    );
+    const tables = currentAllocationsTab.find(".tableListBlock");
+    const holdings = _.flatten(
+      tables.map((_i, el) => getHoldingsFromTable($, $(el)))
+    );
+    // console.log(JSON.stringify({ tabName, holdings }));
+    return holdings;
+  });
   //   console.log(
   //     // currentHoldingsTab.map(function () {
   //     //   return cheerio.load(this).html();
   //     // })
   //     currentAllocationsTab.html()
   //   );
-  return [];
+  // const allHoldings = holdings;
+  return _.flatten(allHoldings);
 }
 
-function main() {
-  getHoldingsPage().then((page) => parseHoldings(page));
+async function main() {
+  const holdings = await getHoldingsPage().then((page) => parseHoldings(page));
+  console.log(JSON.stringify(holdings));
 }
 
 main();
